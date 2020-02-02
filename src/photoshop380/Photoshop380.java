bcf47d419c34ed960e1c96883ff45ad663d6fb8c
Ahmed Sultan Asiri <ia7mad13@gmail.com>
2020-02-02 23:40:36 +0300

    Manuplating with Slider RGB Colors DONE

--- src/photoshop380/Photoshop380.java
+++ src/photoshop380/Photoshop380.java
@@ -17,7 +17,7 @@ public class Photoshop380 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        new MainPage().setVisible(true);
     }
     
 }
