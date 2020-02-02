0ddeb4aa1b1424c651f570569885dc8cada25d13
Ahmed Sultan Asiri <ia7mad13@gmail.com>
2020-02-02 12:35:57 +0300

    starter code

---
+++ src/photoshop380/Photoshop380.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package photoshop380;
+
+import AhmadClassCPIT380.Picture;
+
+/**
+ *
+ * @author ia7ma
+ */
+public class Photoshop380 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
