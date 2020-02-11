8c95961defdf9e6956c7152463eb081544342d51
Ahmed Sultan Asiri <ia7mad13@gmail.com>
2020-02-12 01:37:39 +0300

    Adding Horizontal Reflection.

--- src/photoshop380/PictureUI.java
+++ src/photoshop380/PictureUI.java
@@ -74,7 +74,7 @@ public class PictureUI extends javax.swing.JFrame {
         jButton4 = new javax.swing.JButton();
         jPanel7 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
-        jButton5 = new javax.swing.JButton();
+        horizantalRefliction = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
         jButton8 = new javax.swing.JButton();
@@ -337,7 +337,12 @@ public class PictureUI extends javax.swing.JFrame {
         jLabel6.setForeground(new java.awt.Color(0, 0, 0));
         jLabel6.setText("Reflection");
 
-        jButton5.setText("Horizantal");
+        horizantalRefliction.setText("Horizantal");
+        horizantalRefliction.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                horizantalReflictionActionPerformed(evt);
+            }
+        });
 
         jButton6.setText("Vertical");
 
@@ -357,7 +362,7 @@ public class PictureUI extends javax.swing.JFrame {
                 .addGap(79, 79, 79)
                 .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(horizantalRefliction, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(81, 81, 81)
                 .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -371,7 +376,7 @@ public class PictureUI extends javax.swing.JFrame {
                 .addComponent(jLabel6)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton5)
+                    .addComponent(horizantalRefliction)
                     .addComponent(jButton6))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -556,6 +561,24 @@ public void reset(){
         icon = new ImageIcon(p.getImage());
         picView.setIcon(icon);
     }//GEN-LAST:event_rotate90LeftActionPerformed
+
+    private void horizantalReflictionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_horizantalReflictionActionPerformed
+        // TODO add your handling code here:
+        Picture newPic = new Picture(pic);
+        Pixel sourcPixel = null;
+        Pixel targetPixel = null;
+        
+        for (int x = 0; x < pic.getWidth()/2; x++) {
+            for (int y = 0; y < pic.getHeight(); y++) {
+                sourcPixel = pic.getPixel(x, y);
+                targetPixel = newPic.getPixel(pic.getWidth() - x - 1, y);
+                targetPixel.setColor(sourcPixel.getColor());
+            }
+        }
+        icon = new ImageIcon(newPic.getImage());
+        picView.setIcon(icon);
+        
+    }//GEN-LAST:event_horizantalReflictionActionPerformed
     /**
      * @param args the command line arguments
      */
@@ -642,10 +665,10 @@ public void reset(){
     private javax.swing.JButton grayScale;
     private javax.swing.JLabel greenLabel;
     private javax.swing.JSlider greenSlider;
+    private javax.swing.JButton horizantalRefliction;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
